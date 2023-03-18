b9c8e93b40a61544b6e389487e46a79283718a73
JSand273 <116413216+JSand273@users.noreply.github.com>
2023-03-18 10:55:32 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,94 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+int main(){
+    
+    
+    /*int n, factorial=1;
+    
+    cout << " Ingrese un numero ";cin>>n;
+    if(n>0){
+    if(n!=0){
+        for(int i=1;i<=n;i++){
+        factorial=factorial*i;
+    }
+    cout<<"su factotial ese:"<<factorial;
+    }
+    else{
+        cout<<"el factotial es: "<<1;
+    }
+    }
+    else{
+        cout<<"no se puede calcular el factorial ingrese numero positivo";
+    }*/
+    
+    
+    
+    /*int n;
+cout<<"Ingrese un numero:";cin>>n;
+if(n%2==0){
+    cout<<"el numero es par y su resultado es:"<<n/2;
+}else{
+    cout<<"el numero es impar y no se puede dividir";
+}*/
+
+
+
+/*int numero, m=2;
+bool band = true;
+cout<< "Ingrese un numero porfavor: ";
+cin>>numero;
+
+while(band && m < numero){
+    if(numero % m==0)
+    band=false;
+    else
+    m=m+1;
+}
+if(band)
+cout<<"El numero es primo";
+else
+cout<< "El numero NO es primo";*/
+
+
+
+/*int multiplicacion, numero, a=1;
+cout<<"Ingrese el numero:";
+cin>>numero;
+
+cout<<"La tabla del "<<numero<<" es  :"<<endl;
+do{
+    multiplicacion = numero * a;
+    
+    cout<<numero<<" x "<< a <<" x "<< multiplicacion <<endl;
+    a++;
+}
+while(a<=10);*/
+
+
+
+/*int n, val, pre = 0, pos = 1;
+
+cout<< "Ingrese un numero porfavor: ";
+cin>>n;
+
+cout<< pre << "\t" << pos << "\t";
+
+for(int x = 3; x <=n; x++ ){
+    val = pre + pos;
+    pre = pos;
+    pos = val;
+    cout<< val << "\t";
+}*/
+
+
+    return 0;
+}
